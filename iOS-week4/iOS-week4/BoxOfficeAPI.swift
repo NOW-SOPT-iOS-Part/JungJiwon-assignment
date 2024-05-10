de67064f4cd25d9209945d1ebc9d0e657a0d778c
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-05-10 21:12:23 +0900

    Feat: TargetType 생성

---
+++ iOS-week4/iOS-week4/BoxOfficeAPI.swift
@@ -0,0 +1,41 @@
+//
+//  BoxOfficeAPI.swift
+//  iOS-week4
+//
+//  Created by  정지원 on 2024/05/10.
+//
+
+import Foundation
+import Moya
+
+enum BoxOfficeAPI {
+    case dailyBoxOfficeList(targetDt: String)
+}
+
+extension BoxOfficeAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice")!
+    }
+    
+    var path: String {
+        switch self {
+            case .dailyBoxOfficeList:
+                return "/searchDailyBoxOfficeList.xml"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Task {
+        switch self {
+            case .dailyBoxOfficeList(let targetDt):
+                return .requestParameters(parameters: ["key": "f5eef3421c602c6cb7ea224104795888", "targetDt": targetDt], encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
