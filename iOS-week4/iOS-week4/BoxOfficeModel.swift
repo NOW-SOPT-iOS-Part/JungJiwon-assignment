6a1d2155fe49b2be32819c1526a84a821e260949
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-05-10 21:10:34 +0900

    Feat: BoxOfficeModel 생성

---
+++ iOS-week4/iOS-week4/BoxOfficeModel.swift
@@ -0,0 +1,15 @@
+//
+//  BoxOfficeModel.swift
+//  iOS-week4
+//
+//  Created by  정지원 on 2024/05/10.
+//
+
+import Foundation
+
+struct BoxOfficeModel: Codable {
+    let rank: Int
+    let movieNm: String
+    let openDt: String
+}
+
